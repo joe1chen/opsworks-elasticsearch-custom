4a2c418772ea539eee4812d0f35e5b918d249e8f
Joseph Chen <joe1chen@hotmail.com>
2014-05-23 15:07:27 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of opsworks-elasticsearch-custom
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'opsworks-elasticsearch-custom'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures opsworks-elasticsearch-custom'
+long_description 'Installs/Configures opsworks-elasticsearch-custom'
+version          '0.1.0'
+
